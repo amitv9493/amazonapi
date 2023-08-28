747600d73054a801d0ce82caf950ce7e46499f5a
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-08-28 11:29:10 +0530

    Initial Commit

--- amazonapi/urls.py
+++ amazonapi/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("auth/", views.authorize, name="auth"),
+    path("login/", views.login, name="login"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.shortcuts import HttpResponse
+import os
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate
+
 
 # Create your views here.
-
-
+# @login_required(login_url="login")
 def home(request):
+    # if request.user.is_authenticated:
     return render(request, "index.html")
+
+    # return redirect("login")
 
 
 def authorize(request):
     return HttpResponse("hello")
+
+
+def login(request):
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+    print(request.user)
+    user = authenticate(username=username, password=password)
+    print(user)
+    if user:
+        return redirect("home")
+
+    else:
+        return render(request, "login.html")
