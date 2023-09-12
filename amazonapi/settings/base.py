23c99598df46c123a87f3378bd7fbd6b42d0bdfa
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-09-12 11:42:31 +0530

    Update the base settings

--- amazonapi/settings/base.py
+++ amazonapi/settings/base.py
@@ -21,9 +21,11 @@ INSTALLED_APPS = [
     "usercredential",
     "users",
     "authapi",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -96,3 +98,12 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated"
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+]
+CORS_ALLOW_ALL_ORIGINS = False
