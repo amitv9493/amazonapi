83b72fb2b1da8369449cba58c6a9adfe0a0caf93
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-09-13 14:23:03 +0530

    update cache backend in prod

--- amazonapi/settings/prod.py
+++ amazonapi/settings/prod.py
@@ -17,3 +17,13 @@ STATIC_URL = "static/"
 MEDIA_URL = "media/"
 
 STATIC_ROOT = '/home/shipping/domains/shippingmart.co.uk/public_html/static'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'redis_cache.RedisCache',
+        'LOCATION': '/home/shipping/.redis/redis.sock',
+    },
+}
+
+# SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
