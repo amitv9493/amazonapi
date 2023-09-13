d8817fd6a9e8fe54021695703716ca8605338b55
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-09-13 13:20:50 +0530

    update prod settings

--- amazonapi/settings/prod.py
+++ amazonapi/settings/prod.py
@@ -3,9 +3,13 @@ DEBUG = False
 ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': os.environ.get('db_name'),
+        'USER': os.environ.get('user'),
+        'PASSWORD': os.environ.get('password'),
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
