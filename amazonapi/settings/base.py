9ce1f844736c1e5c281821ca6cec3db9edf32b07
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-09-26 10:01:51 +0530

    Update settings

--- amazonapi/settings/base.py
+++ amazonapi/settings/base.py
@@ -100,13 +100,6 @@ REST_FRAMEWORK = {
     ]
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000",
-]
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ORIGIN_ALLOW_ALL = True
