ea1394ea9783c72b5db9d443fa310f43c0ca76d3
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-09-26 11:35:33 +0530

    Update settings

--- amazonapi/settings/prod.py
+++ amazonapi/settings/prod.py
@@ -16,6 +16,7 @@ DATABASES = {
 STATIC_URL = "static/"
 MEDIA_URL = "media/"
 
+
 STATIC_ROOT = '/home/shipping/domains/shippingmart.co.uk/public_html/static'
 
 CACHES = {
@@ -30,11 +31,26 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 
 
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_RENDERER_CLASSES': (
-#         'rest_framework.renderers.JSONRenderer',
-#     )
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
 
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = True
 
+
+SESSION_COOKIE_SAMESITE = 'Lax'  # Adjust for development only
+CSRF_COOKIE_SAMESITE = 'Lax'
+# SESSION_COOKIE_DOMAIN = 'localhost:3000/'
+CSRF_COOKIE_HTTPONLY =False
+SESSION_COOKIE_HTTPONLY = False
+
+# # PROD ONLY
+# # CSRF_COOKIE_SECURE = True
+# # SESSION_COOKIE_SECURE = True
+
+CORS_EXPOSE_HEADERS = ['Content-Type', 'X-CSRFToken', 'Set-Cookie']
+CORS_ALLOW_CREDENTIALS = True
