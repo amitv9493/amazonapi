e966a9ddd5e4c3e281e84942d82f96223b81cc89
Amit Patel <67977027+amitv9493@users.noreply.github.com>
2023-09-26 13:59:03 +0530

    Update JWT endpoints.

--- authapi/urls.py
+++ authapi/urls.py
@@ -6,8 +6,8 @@ from rest_framework_simplejwt.views import (
 )
 urlpatterns = [
     path("csrf", views.GetCSRFToken.as_view(), name="csrf"),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
     
     path("login", views.LoginView.as_view(), name="login"),
